536f86ec8e494c1df810e21df786184bba9b7b87
Joebert Cerezo <joebertcerezo@gmail.com>
2026-09-20 06:32:52 +0800

    feat: add helper functions for system information retrieval

--- sysinfo.sh
+++ sysinfo.sh
@@ -1,1 +1,55 @@
 #!/usr/bin/env bash
+
+set -u
+
+DMI="/sys/devices/virtual/dmi/id"
+
+# ============================================================
+# HELPERS
+# ============================================================
+
+get_file() {
+    local file="$1"
+
+    if [[ -r "$file" ]]; then
+        local value
+        value=$(<"$file")
+        [[ -n "$value" ]] && printf '%s' "$value" || printf '%s' "N/A"
+    else
+        printf '%s' "N/A"
+    fi
+}
+
+field() {
+    printf '%-20s : %s\n' "$1" "$2"
+}
+
+section() {
+    printf '\n'
+    printf '========================================\n'
+    printf '%27s\n' "$1"
+    printf '========================================\n'
+}
+
+subsection() {
+    printf '\n'
+    printf '%s\n' "$1"
+    printf '%s\n' "----------------------------------------"
+}
+
+format_gib() {
+    awk -v bytes="$1" '
+        BEGIN {
+            printf "%.1f GiB", bytes / 1024 / 1024 / 1024
+        }
+    '
+}
+
+format_khz() {
+    awk -v khz="$1" '
+        BEGIN {
+            printf "%.2f GHz", khz / 1000000
+        }
+    '
+}
+
